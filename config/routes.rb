e5ab2d215707ed77e6a720a138d8895e4f864eb5
Karim Timer <abdikariim.tim>
2018-11-05 14:48:52 +0000

    date, image url and navbar fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-get 'about' => 'pages#about', as: :about
-get 'Contact' => 'pages#contact', as: :contact
+  get 'about' => 'pages#about', as: :about
+  get 'Contact' => 'pages#contact', as: :contact
 
-	root to: 'posts#index'
+  root to: 'posts#index'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
