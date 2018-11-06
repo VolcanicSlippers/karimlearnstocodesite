62f2f5ad795fcb7bc33f9469a445f05421d159cf
Karim Timer <abdikariim.tim>
2018-11-06 14:59:25 +0000

    basic features completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
-  get 'about' => 'pages#about', as: :about
-  get 'Contact' => 'pages#contact', as: :contact
 
-  root to: 'posts#index'
-  resources :posts
+
+  root to: 'blog/posts#index'
+#expecting the actual name in the url, rather than what is seen is scope.
+namespace :author do
+ resources :posts	
+end
+
+scope module: 'blog' do
+  get 'about' => 'pages#about', as: :about
+  get 'contact' => 'pages#contact', as: :contact
+  get 'posts' => 'posts#index', as: :posts
+  get 'posts/:id' => 'posts#show', as: :post
+
+
+  # 'posts/kassd' => params[:id] == 'kassd' 
+
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
