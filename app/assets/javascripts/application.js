19262466b52f9db35c6394de326c574430f09a9f
Karim Timer <abdikariim.tim>
2018-11-07 16:50:10 +0000

    rich text editor added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,12 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("turbolinks:load", function(){
+	tinymce.remove();
+	tinymce.init({ 
+		selector: 'textarea#post_body',
+		plugins: "codesample image media link code",
+  		toolbar: "undo redo | styleselect | bold italics link | codesample image media | code",
+	});
+})
